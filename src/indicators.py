cbca084b74f88ef893682517fe9109aa7569858f
Viking Production <154587935+VikingProduction@users.noreply.github.com>
2025-08-31 21:32:29 +0200

    Empower

--- src/indicators.py
+++ src/indicators.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import ta
 
-def compute_indicators(df: pd.DataFrame) -> pd.DataFrame:
-    # SMA courts et longs
-    df['SMA_short'] = ta.trend.SMAIndicator(df['close'], window=10).sma_indicator()
-    df['SMA_long']  = ta.trend.SMAIndicator(df['close'], window=50).sma_indicator()
-    # RSI
-    df['RSI']       = ta.momentum.RSIIndicator(df['close'], window=14).rsi()
+def compute_indicators(df: pd.DataFrame, config: dict) -> pd.DataFrame:
+    rsi_w = config['indicators']['rsi_window']
+    sma_s = config['indicators']['sma_short_window']
+    sma_l = config['indicators']['sma_long_window']
+    df['SMA_short'] = ta.trend.SMAIndicator(df['close'], window=sma_s).sma_indicator()
+    df['SMA_long']  = ta.trend.SMAIndicator(df['close'], window=sma_l).sma_indicator()
+    df['RSI']       = ta.momentum.RSIIndicator(df['close'], window=rsi_w).rsi()
     return df
